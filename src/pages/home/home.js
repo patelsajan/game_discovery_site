6334f0d95c6f959b144048909ed69af9b7754696
patelsajan <patelsajan86@gmail.com>
2023-06-07 23:49:52 +0530

    some styling changes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,6 +5,7 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div className="header">
+				<div className="header_text">Free to play games</div>		
 				<div className="menu_btn"><img src={require("../../assets/menu.png")} alt="menu button" /></div>
 			</div>
 		);
